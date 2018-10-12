8d8f9cab6f4ee331a1303949963f788586c544c5
Cédric <CedricG44@users.noreply.github.com>
2018-10-12 17:13:24 +0200

    Serializable model

--- src/main/java/imt/org/web/commonmodel/Measure.java
+++ src/main/java/imt/org/web/commonmodel/Measure.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @AllArgsConstructor
-public class Measure {
+public class Measure implements Serializable {
 
     private double temperature;
     private double windSpeed;
--- src/main/java/imt/org/web/commonmodel/SensorData.java
+++ src/main/java/imt/org/web/commonmodel/SensorData.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.sql.Timestamp;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class SensorData {
+public class SensorData implements Serializable {
 
     private int idSensor;
     private String idCountry;
