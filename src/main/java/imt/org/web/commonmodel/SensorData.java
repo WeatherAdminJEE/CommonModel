064469c5f7677d2559d4024cd8045cdfb0183399
Cédric <CedricG44@users.noreply.github.com>
2018-10-12 08:39:51 +0200

    Add test class

---
+++ src/main/java/imt/org/web/commonmodel/SensorData.java
@@ -0,0 +1,13 @@
+package imt.org.web.commonmodel;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SensorData {
+
+    private int test;
+}
